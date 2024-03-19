b7824153dc943fad199adb2a5a7f1b99a7ca5a78
terragul <Denislukianov45@gmail.com>
2024-03-19 21:51:11 +0300

    noob

---
+++ p3265/p3265/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using p3265.p3DataSetTableAdapters;
+
+namespace p3265
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void left(object sender, RoutedEventArgs e)
+        {
+            datatata datatatatatata = new datatata();
+            datatatatatata.Show();
+            Close();
+        }
+
+        private void right(object sender, RoutedEventArgs e)
+        {
+            vtoroeokno supervtoroe = new vtoroeokno();
+            supervtoroe.Show();
+            Close();
+        }
+
+    }
+}
---
+++ p3265/p3265/vtoroeokno.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace p3265
+{
+    /// <summary>
+    /// Логика взаимодействия для vtoroeokno.xaml
+    /// </summary>
+    public partial class vtoroeokno : Window
+    {
+        p3Entities context = new p3Entities();
+        public vtoroeokno()
+        {
+            InitializeComponent();
+
+            vtorayatabl.ItemsSource = context.Courses.ToList();
+        }
+    }
+}
